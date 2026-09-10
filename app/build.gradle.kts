e9acf4d7ad47ade13ccb1b8c01d46a415ee3b541
948872182zz-hash <948872182zz@gmail.com>
2026-09-10 15:34:59 +0800

    Mark Android app as 2.0 series

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = buildNumber
-        versionName = "0.1.$buildNumber"
+        versionName = "2.0.$buildNumber"
     }
 
     buildTypes {
